da4e1d83a0e42ac069d2ecad9cb46ee2f50a549b
Luciano <lucianoarias1997@gmail.com>
2024-01-26 00:12:51 -0300

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import LandingHero from '@/components/landing-hero';
+import LandingNavbar from '@/components/landing-navbar';
+
+export default function LandingPage() {
+  return (
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+    </div>
+  );
+}
